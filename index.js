342ed676c7d428f6f718c9cbc44784428442052c
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-03-02 18:24:35 -0500

    Add patch request using method-override

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var methodOverride = require('method-override')
 const { v4: uuid } = require('uuid');
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'))
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
