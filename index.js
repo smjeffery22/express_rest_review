78c5e07708a4e1a4c6de63ccebd9ac5874dd79f0
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-03-02 16:33:52 -0500

    FIrst commit - set up express

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/tacos', (req, res) => {
+  res.send('GET /tacos response');
+})
+
+app.post('/tacos', (req, res) => {
+  const { meat, qty } = req.body;
+  res.send(`Order: ${qty} ${meat} tacos`);
+})
+
+app.listen(3000, () => {
+  console.log('RUNNING ON PORT 3000!');
+})
