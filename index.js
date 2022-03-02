da7f004031eb7d3330a7a6676a7670bb45c49d52
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-03-02 16:40:18 -0500

    Add json parse express middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos response');
