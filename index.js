934121f7c3b459ab46b07c34ebb4c48d481c0cf4
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-03-02 16:59:04 -0500

    Add and set up ejs, and add fake data

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', ejs);
+
+let comments = [
+  {
+    username: 'Todd',
+    comment: 'lol, that is so funny'
+  },
+  {
+    username: 'Skyler',
+    comment: 'I like to go birdwatching with my dog'
+  },
+  {
+    id: uuid(),
+    username: 'Sk8erBoi',
+    comment: 'Please delete your account, Todd'
+  },
+  {
+    username: 'onlysayswoof',
+    comment: 'woof woof woof'
+  }
+];
 
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos response');
